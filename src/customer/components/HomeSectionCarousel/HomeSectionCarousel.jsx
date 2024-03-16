f32207e0cfd5e24cce6ace192f0dd74b04a3a899
Ayush_Shukla <ayush_shukla@Ninja-ITs-MacBook-Air.local>
2024-03-16 16:36:52 +0530

    Added footer

--- src/customer/components/HomeSectionCarousel/HomeSectionCarousel.jsx
+++ src/customer/components/HomeSectionCarousel/HomeSectionCarousel.jsx
@@ -3,9 +3,8 @@ import AliceCarousel from "react-alice-carousel";
 import HomeSectionCard from "../HomeSectionCard/HomeSectionCard";
 import { Button } from "@mui/material";
 import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
-import { mens_kurta } from "../../../Data/Mens_kurta";
 
-const HomeSectionCarousel = () => {
+const HomeSectionCarousel = ({ data , sectionName}) => {
   const [activeIndex, setActiveIndex] = useState(0);
 
   const responsive = {
@@ -14,24 +13,23 @@ const HomeSectionCarousel = () => {
     1024: { items: 5.5 },
   };
 
-  const slidePrev = () => {
-    setActiveIndex(activeIndex - 1);
-  };
+  const slidePrev = () => setActiveIndex(activeIndex - 1);
 
   const slideNext = () => {
     setActiveIndex(activeIndex + 1);
   };
 
-  const syncActiveChange = (item) => {
+  const syncActiveChange = ({ item }) => {
     setActiveIndex(item);
   };
 
-  const items = mens_kurta
+  const items = data
     .slice(0, 10)
     .map((item) => <HomeSectionCard product={item} />);
 
   return (
     <div className="">
+        <h2 className="text-2xl font-extrabold text-gray-800">{sectionName}</h2>
       <div className="relative p-5">
         <AliceCarousel
           items={items}
@@ -40,7 +38,7 @@ const HomeSectionCarousel = () => {
           autoPlayInterval={1000}
           responsive={responsive}
           disableDotsControls
-          onSlideChange={syncActiveChange}
+          onSlideChanged={syncActiveChange}
           activeIndex={activeIndex}
         />
         {activeIndex !== items.length - 5 && (
@@ -62,23 +60,25 @@ const HomeSectionCarousel = () => {
             />
           </Button>
         )}
-        <Button
-          variant="contained"
-          className="z-50"
-          onClick={slidePrev}
-          sx={{
-            position: "absolute",
-            top: "8rem",
-            left: "0rem",
-            transform: "translateX(-50%) rotate(-90deg)",
-            bgcolor: "white",
-          }}
-          aria-label="next"
-        >
-          <KeyboardArrowLeftIcon
-            sx={{ transform: "rotate(90deg)", color: "black" }}
-          />
-        </Button>
+        {activeIndex !== 0 && (
+          <Button
+            variant="contained"
+            className="z-50"
+            onClick={slidePrev}
+            sx={{
+              position: "absolute",
+              top: "8rem",
+              left: "0rem",
+              transform: "translateX(-50%) rotate(-90deg)",
+              bgcolor: "white",
+            }}
+            aria-label="next"
+          >
+            <KeyboardArrowLeftIcon
+              sx={{ transform: "rotate(90deg)", color: "black" }}
+            />
+          </Button>
+        )}
       </div>
     </div>
   );
